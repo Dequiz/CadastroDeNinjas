6fca96527fd3d9a96e4603196ad2867d750633b9
André Holovati <andreholovati@gmail.com>
2026-02-19 01:51:50 -0300

    Crição do Controller

---
+++ src/main/java/dev/Deco/CadastroDeNinjas/Controller/ninjaController.java
@@ -0,0 +1,18 @@
+package dev.Deco.CadastroDeNinjas.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ninjaController {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem";
+    }
+
+
+}
